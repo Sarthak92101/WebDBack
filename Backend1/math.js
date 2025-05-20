fa5ea1e03293cabd2f1aef0b46b3c21c69459abf
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-05-20 16:16:02 +0530

    Initial commit

---
+++ Backend1/math.js
@@ -0,0 +1,17 @@
+// const sum=(a,b) => a+b;
+// const mul=(a,b) => a*b;
+// const PI=3.14;
+
+// module.exports=123;
+
+// let obj={
+//   sum: sum,
+//   mul: mul,
+//   PI: PI,
+// };
+
+// module.exports=obj
+
+export const sum=(a,b) => a+b;
+export const mul=(a,b) => a*b;
+export const PI=3.14;
---
+++ Backend1/script.js
@@ -0,0 +1,29 @@
+// let n=5;
+
+// for(let i=0; i<=n; i++){
+//   console.log("Hello" ,i);
+// }
+
+// let args=process.argv;
+
+// for(let i=2; i<args.length; i++){
+//   console.log("Heyy to ", args[i])
+// }
+
+// const math=require("./math")
+
+// console.log(math)
+
+
+
+//IMPORT DIRECTORY FRUITS
+// const info=require("./Fruits");
+// console.log(info)
+
+
+//IMPORT STATEMENT
+
+  import{sum,PI} from "./math.js";
+
+  console.log(sum(1,2));
+  console.log(PI)
